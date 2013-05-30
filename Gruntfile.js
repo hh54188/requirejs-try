66075adfb35820cfe703a222bc1d9e6e07ab4b45
hh54188 <hh54188@yahoo.cn>
2013-05-30 17:42:10 +0800

    add some comment

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,11 +3,13 @@ module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         requirejs: {
-            compile: {
+            compileSingle: {
               options: {
                 baseUrl: "public/js",
                 paths: {
-                    jquery: "lib/jquery-1.10.0.min" // 不支持main.js中的数组形式，不支持多个地址，不支持url
+                    jquery: "lib/jquery-1.10.0.min" 
+                    // 不支持main.js中的数组形式，不支持多个地址，不支持url
+                    // 如果config中使用的是URL，则jquery: 'empty'
                 },                
                 name: "main",
                 out: "main-min.js"
